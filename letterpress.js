2f57bc93ca1d13f42329312ffc18d44415aeedc0
Neil Kandalgaonkar <neilk@brevity.org>
2012-11-13 00:57:32 -0800

    most of letterpress script turned into JS, however dictionary reading is async still, so it will not work until we use eventemitter. Added lazy module

--- letterpress.js
+++ letterpress.js
@@ -1,33 +1,110 @@
 var fs = require('fs');
 var path = require('path');
 var util = require('util');
+var lazy = require('lazy');
 
 
 // set up cache directory
 var appDir = path.dirname(process.argv[1]);
 
 var cacheDir = getCacheDir(appDir);
+var wordFile = path.join(appDir, 'words.txt');
 // command line arguments
 // argv[0] is executing binary, argv[1] is scriptname
 var commandLineArgs = process.argv.slice(2);
 var board = letterStringToCanonicalArray(commandLineArgs[0]);
-var desired = letterStringToCanonicalArray(commandLineArgs[1]);
 
-
+if (board.length !== 25) {
+  console.log("board must have 25 letters, instead has " + board.length);
+  console.log(board);
+  process.exit(1);
+}
+var desired = [];
+if (typeof commandLineArgs[1] !== null) {
+  desired = letterStringToCanonicalArray(commandLineArgs[1]);
+}
 console.log(board);
 console.log(desired);
 
 
+
 /**
- * Take a string, return sorted array of characters
+ * Given a board, determine all the words that could possibly go onto it
+ * Relies on global variables cacheDir and wordFile
+ * @param {Array} of chars
+ * @return {Array} of {Array} of chars, representing words
+ */
+function getPossibleWords(board) {
+  var words = [];
+  var cachePath = path.join(cacheDir, board.join(""));
+  /*if (fs.existsSync(cachePath)) {
+    words = fs.readFileSync(cachePath);
+  } else {
+  */
+
+    var words;
+    function checkLine(line) {
+      var wordStr = line.toString();
+      wordStr = wordStr.replace(/\n/g, '');
+      var word = letterStringToCanonicalArray(wordStr);
+      if (isSubset(board, word)) {
+        words.push([wordStr, word]);
+      }
+    }
+
+    console.log("about to read...");
+    var stream = fs.createReadStream(wordFile);
+    var lazyReader = new lazy(stream).lines.forEach(checkLine);
+    console.log("after read...");
+//    fs.writeFileSync(cachePath, words);
+  // }
+  return words;
+}
+
+/**
+ * Given two sorted arrays of chars, determine if 2nd is subset of 1st
+ * @param {Array} of chars
+ * @param {Array} of chars
+ * @return {Boolean}
+ */
+function isSubset(set, sub) {
+  for (var setI = 0, subI = 0; setI < set.length && subI < sub.length; setI += 1) {
+    if (set[setI] === sub[subI]) {
+      subI++;
+    }
+  }
+  return subI == sub.length;
+}
+
+/**
+ * Return function to determine if a word has all desired characters
+ * @param {Array} of chars - desired chars
+ * @return {Function}
+ */
+function getDesiredMatcher(desired) {
+  /**
+   * Checks if a given word has all the desired characters
+   * @param {Array} of chars
+   * @return {Boolean}
+   */
+  return function(word) {
+    return isSubset(word, desired);
+  }
+}
+
+
+/**
+ * Take a sorted array of characters
  * @param {String} letters
  * @return {Array} of single character strings
  */
 function letterStringToCanonicalArray(letterStr) {
-  letterStr.replace(/\W/g, '');
   var arr = [];
-  for (var i = 0; i < letterStr.length; i += 1) {
-    arr.push(letterStr[i]);
+  if (typeof letterStr === 'string') {
+    letterStr = letterStr.replace(/\W/g, '');
+    for (var i = 0; i < letterStr.length; i += 1) {
+      arr.push(letterStr[i]);
+    }
   }
   return arr.sort();
 }
@@ -38,7 +115,7 @@ function letterStringToCanonicalArray(letterStr) {
  * Actually, this should all be memcacheable
  * Exits with error if cannot do so
  * @param {String} directory appDir
- * @param {String} cache directory
+ * @param {String} directory cacheDir
  */
 function getCacheDir(appDir) {
   var cacheDir = path.join(appDir, '.letterPressCache');
@@ -58,4 +135,29 @@ function getCacheDir(appDir) {
   return cacheDir;
 }
 
+/**
+ * Comparator for length of strings
+ * @param {String}
+ * @param {String}
+ * @return {Number}
+ */
+function byLengthDescending(a, b) {
+  return b.length - a.length;
+}
 
+
+var wordStrs = [];
+var isDesired = getDesiredMatcher(desired);
+var possibleWordTuples = getPossibleWords(board);
+for (var i = 0; i < possibleWordsTuples.length; i += 1) {
+  var wordStr = possibleWords[i][0];
+  var word = possibleWords[i][1];
+  if (isDesired(word)) {
+    wordStrs.push(wordStr);
+  }
+}
+
+if (wordStrs.length) {
+  // sort
+  console.log(( wordStrs.sort(byLengthDescending) ).join(" "));
+}
