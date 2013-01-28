302b870f64356f9588f7963abeb6a5162b679818
Neil Kandalgaonkar <neilk@brevity.org>
2013-01-27 18:55:32 -0800

    first cut at one input per letter

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,18 @@
-(function(){
+(function($){
   $('#minFrequencySlider').slider({
     value:16,
     min:0,
     max:24
   });
+
+  $('#getBoard input[type=text]')
+    .click( function() { $(this).select(); } )
+    .keyup( function(event) {
+      // this form has tabIndexes 1-25 for the inputs. Submit button is 26.
+      if (event.which !== 9) {
+        var nextTabIndex = (parseInt(this.tabIndex, 10) + 1).toString()
+        $(this.form).find('[tabIndex=' + nextTabIndex.toString() + ']').focus().select();
+      }
+    });
+
 })(jQuery);
