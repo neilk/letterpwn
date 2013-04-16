9af14dff9d0d8e65df64b1c5d02b80de8e145926
Neil Kandalgaonkar <neilk@brevity.org>
2013-04-16 08:59:15 -0700

    test live

--- test/stress.pl
+++ test/stress.pl
@@ -1,10 +1,13 @@
 #!/usr/bin/env perl -w
 
 use strict;
+
+my $host = $ARGV[0] || 'localhost:3000';
+
 while(1) { 
   my $b = join "", map { chr(97 + (int(rand(26)))) } (0..24);
   print "$b\n";
-  my $url = "http://localhost:3000/api?seq=1&board=$b&minFrequency=0&oursBitMask=0&theirsBitMask=0";
+  my $url = "http://$host/api?seq=1&board=$b&minFrequency=0&oursBitMask=0&theirsBitMask=0";
   print `curl -sS '$url'`;
 }
 
