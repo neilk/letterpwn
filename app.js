b5590c33eec39769a6641c60578118f76f9f25be
Neil Kandalgaonkar <neilk@brevity.org>
2013-04-23 18:37:52 -0700

    initial nodetime config

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var express = require('express')
     //testRoute = require('./routes/test')
     path = require('path');
 
+console.log(process.env.NODETIME_ACCOUNT_KEY);
+if(process.env.NODETIME_ACCOUNT_KEY) {
+  require('nodetime').profile({
+    accountKey: process.env.NODETIME_ACCOUNT_KEY,
+    appName: 'LetterPwn' // optional
+  });
+}
+
 var app = express();
 
 app.configure(function(){
